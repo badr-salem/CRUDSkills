17e77734db8fbb5b59fb06f62a4f221ffcbf570c
badrs <badrsalem@gmail.com>
2024-07-01 12:20:58 +0300

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CRUDSkills.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDSkills.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options)
+		{
+		}
+
+		public DbSet<Skill> skills { get; set; }
+	
+
+
+	}
+}
---
+++ Models/Skill.cs
@@ -0,0 +1,8 @@
+﻿namespace CRUDSkills.Models
+{
+	public class Skill
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+	}
+}
