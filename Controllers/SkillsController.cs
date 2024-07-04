c3c7ce819ca80e2bd9ebe2547f6a21ce4e91ecad
badrs <badrsalem@gmail.com>
2024-07-04 12:59:00 +0300

    CreateAndUpdateSkill

--- Controllers/SkillsController.cs
+++ Controllers/SkillsController.cs
@@ -1,4 +1,5 @@
 ﻿using CRUDSkills.Data;
+using CRUDSkills.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CRUDSkills.Controllers
@@ -15,6 +16,87 @@ namespace CRUDSkills.Controllers
         {
             var list = _context.skills.ToList();
             return View(list);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Skill skill)
+        {
+
+            if (ModelState.IsValid){
+
+                var skillFromDb = _context.skills.FirstOrDefault(i => i.Name == skill.Name);
+
+
+
+
+                if (skillFromDb == null)
+                {
+                    _context.skills.Add(skill);
+                    _context.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ModelState.AddModelError("Name",
+                                 "إسم المهارة موجود مسبقا");
+                    return View(skill);
+                }
+
+
+                
+            }
+            {
+                return View(skill);
+            }
+
+            
+
+           
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            var skillFromDb = _context.skills.FirstOrDefault(i => i.Id == id);
+            if(skillFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(skillFromDb);
+
+
+        }
+
+        [HttpPost]
+        public IActionResult Update(Skill skill)
+        {
+            if (ModelState.IsValid)
+            {
+                var skillFromDb = _context.skills.FirstOrDefault(i=>i.Name == skill.Name && i.Id != skill.Id);
+                if(skillFromDb != null)
+                {
+                    ModelState.AddModelError("Name",
+                                "إسم المهارة موجود مسبقا");
+                    return View(skill);
+                }
+                else
+                {
+                    _context.skills.Update(skill);
+                    _context.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            else
+            {
+                return View(skill);
+            }
         }
     }
 }
--- Models/Skill.cs
+++ Models/Skill.cs
@@ -1,8 +1,12 @@
-﻿namespace CRUDSkills.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUDSkills.Models
 {
 	public class Skill
 	{
 		public int Id { get; set; }
+
+		[Required(ErrorMessage ="هذا الحقل إجباري")]
 		public string Name { get; set; }
 
 		public bool IsActive { get; set; }
