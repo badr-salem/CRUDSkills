7177cdda73197793c6ddcf0ed2705b8787364377
badrs <badrsalem@gmail.com>
2024-07-08 11:50:27 +0300

    ProjectModel

---
+++ Models/Project.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUDSkills.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="هذا الحقل إلزامي")]
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public string? Image { get; set; }
+    }
+}
